a92517ae6b89993c74e171eb114111ca4402a46f
priya-singh27 <priya.beingdev@gmail.com>
2026-08-13 21:53:39 +0530

    Add database schema for repos and chunks, including table creation and connection handling in db_config.js

--- server/db/db_config.js
+++ server/db/db_config.js
@@ -6,16 +6,45 @@ const pool = new pg.Pool({
     max:10
 });
 
+const createTable = `
+CREATE TABLE IF NOT EXISTS repos(
+   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+   user_id  UUID NOT NULL REFERENCES auth.users(id) ON UPDATE CASCADE ON DELETE CASCADE,
+   owner VARCHAR(255),
+   name VARCHAR(255),
+   github_url TEXT,
+   status TEXT NOT NULL DEFAULT 'pending' CHECK (status IN ('pending', 'indexing', 'ready', 'failed')),
+   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+   updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+   UNIQUE (user_id, owner, name)
+);
+CREATE EXTENSION IF NOT EXISTS vector;
+
+CREATE TABLE IF NOT EXISTS chunks (
+   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+   repo_id UUID NOT NULL REFERENCES repos(id) ON UPDATE CASCADE ON DELETE CASCADE,
+   path TEXT,
+   start_line INTEGER,
+   end_line INTEGER,
+   content TEXT,
+   embedding VECTOR(1536) NOT NULL,
+   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+`;
+
 (
     async function() {
+        let client;
        try{
-            const client = await pool.connect();
-            const result = await client.query('SELECT NOW()');
+             client = await pool.connect();
+            console.log(`DB connected...`);
+            const result = await client.query(createTable);
             // console.log(result.rows[0])
-            console.log(`DB connected...`)
-            client.release();
+            console.log(`Tables created...`);
        }catch(err){
             console.log(err)
+       }finally{
+            client.release();
        }
     }
 )()

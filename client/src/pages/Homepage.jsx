2c5e84e0a71981690044db726d7e960f19ad5690
priya-singh27 <priya.beingdev@gmail.com>
2026-08-19 22:39:07 +0530

    Add input field for GitHub URL on homepage; implement state management for form data using useState hook

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import { useAuth } from "../context/AuthContext"
 
 export default function Homepage(){
 
-    const {user, logout} = useAuth()
+    const {user, logout} = useAuth();
+    const [formData, setFormData] = useState({
+      github_url:""
+    });
+
+    const handleInputChange = (e)=>{
+      const {name, value} = e.target;
+      setFormData((prev)=>({
+        ...prev,
+        [name]:value
+      }))
+    }
     
     return(
         <>
-          <h1>Welcom,{user.email}</h1>
+          <input onChange={handleInputChange} type="url" placeholder="enter your github url..." name="github_url" value={formData.github_url}></input>
           <button onClick={()=>{logout()}}>
             Logout
           </button>
